d69fc5911be5234abdf41a629dc56c728a52692b
ToganDima <dima.togan@gmail.com>
2023-02-24 04:09:31 +0200

    added homework #20

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import HomePage from './components/home/HomePage';
