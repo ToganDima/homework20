4b6431fdff36d07324e1acc250a14eda0c996f9c
ToganDima <dima.togan@gmail.com>
2023-02-24 04:03:19 +0200

    added homework #20

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import HomePage from './components/home/HomePage';
 import Publications from './components/publications/Publications';
@@ -10,20 +10,18 @@ import ContactDetails from './components/contacts/ContactDetails';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/publications" element={<Publications />} />
-            <Route path="/photos" element={<Photos />} />
-            <Route path="/contacts" element={<ContactsBook />} />
-            <Route path="/contacts/:username" element={<ContactDetails />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<HomePage />} />
+          <Route path="publications" element={<Publications />} />
+          <Route path="photos" element={<Photos />} />
+          <Route path="contacts" element={<ContactsBook />} />
+          <Route path="contacts/:username" element={<ContactDetails />} />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "../components/Navbar.css";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="nav-logo">
-        <h1>SPA. React-route</h1>
-      </div>
-      <div className="nav-links">
-        <NavLink className="nav-link" to="/">Home</NavLink>
-        <NavLink className="nav-link" to="/publications">Publications</NavLink>
-        <NavLink className="nav-link" to="/photos">Photos</NavLink>
-        <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
-      </div>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="nav-logo">
+          <h1>SPA. React-route</h1>
+        </div>
+        <div className="nav-links">
+          <NavLink
+            className="nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-link" to="/publications">
+            Publications
+          </NavLink>
+          <NavLink className="nav-link" to="/photos">
+            Photos
+          </NavLink>
+          <NavLink className="nav-link" to="/contacts">
+            Contacts
+          </NavLink>
+        </div>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
